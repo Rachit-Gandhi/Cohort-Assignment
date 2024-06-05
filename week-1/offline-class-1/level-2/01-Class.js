5ddcb9e2b249ccb33938476729f64f43772a8a27
Rachit-Gandhi <rachitgandhi2002@gmail.com>
2024-06-05 15:36:31 +0530

    update 0-level 2 week 1

--- week-1/offline-class-1/level-2/01-Class.js
+++ week-1/offline-class-1/level-2/01-Class.js
@@ -8,5 +8,17 @@ class Animal {
     return `${this.name} has ${this.legCount} legs`
   }
 }
+class ClassName {
+  constructor(member1, member2){
+    this.member1 = member1;
+    this.member2 = member2;
+    return this;
+  }
+  describe(){
+    return '${this.member1} and {this.member2}'
+  }
+}
+let hello = ClassName.constructor("hello1","hello2");
+console.log(hello);
 
 
